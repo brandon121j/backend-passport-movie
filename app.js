dde13c8393b7392611aabaeda0515baae8cf7fea
Brandon <brandon121j@gmail.com>
2021-11-29 15:11:26 -0500

    Checkpoint commit: Setting up application

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
+var mongoose = require('mongoose');
+var passport = require('passport');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
